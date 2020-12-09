655219dcbc23c3c8406449b139046fea50c6f255
alexmaoshijian <maoshijian@gmail.com>
2020-12-09 13:29:38 +0800

    first commit

---
+++ js/osahan.js
@@ -0,0 +1,73 @@
+$("body").on("contextmenu",function(e){
+        return false;
+    });
+$(document).keydown(function(e){
+         if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)){
+           return false;
+         }
+         if(e.which === 123){
+             return false;
+         }
+         if(e.metaKey){
+             return false;
+         }
+         //document.onkeydown = function(e) {
+         // "I" key
+         if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+             return false;
+         }
+         // "J" key
+         if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+             return false;
+         }
+         // "S" key + macOS
+         if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+             return false;
+         }
+         if (e.keyCode == 224 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+             return false;
+         }
+         // "U" key
+         if (e.ctrlKey && e.keyCode == 85) {
+            return false;
+         }
+         // "F12" key
+         if (event.keyCode == 123) {
+            return false;
+         }
+    });
+
+(function($) {
+  "use strict"; // Start of use strict
+  
+// Tooltip
+$('[data-toggle="tooltip"]').tooltip();
+
+// Osahan Slider
+$('.osahan-slider').slick({
+  centerMode: true,
+  centerPadding: '30px',
+  slidesToShow: 2,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+})(jQuery); // End of use strict
